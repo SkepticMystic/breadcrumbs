b59dbd8ff9092c570771e942fef88242b162fa3b
Ross Keenan <rossk29@gmail.com>
2021-06-17 10:17:36 +0200

    Initial setup attempt

--- main.ts
+++ main.ts
@@ -1,112 +1,191 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+  App,
+  Modal,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+  TFile,
+} from "obsidian";
+import { Graph } from "graphlib";
+import * as graphlib from "graphlib";
 
 interface MyPluginSettings {
-	mySetting: string;
+  mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+  mySetting: "default",
+};
+
+interface nameContent {
+  fileName: string;
+  content: string;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+interface childParent {
+  child: string;
+  parent: string;
+}
 
-	async onload() {
-		console.log('loading plugin');
+export default class BreadcrumbsPlugin extends Plugin {
+  settings: MyPluginSettings;
 
-		await this.loadSettings();
+  async onload() {
+    console.log("loading plugin");
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
+    await this.loadSettings();
 
-		this.addStatusBarItem().setText('Status Bar Text');
+    this.addRibbonIcon("dice", "Breadcrumbs", () => {
+      new Notice("This is a notice!");
+    });
 
-		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
-				}
-				return false;
-			}
-		});
+    this.addCommand({
+      id: "open-sample-modal",
+      name: "Open Sample Modal",
+      // callback: () => {
+      // 	console.log('Simple Callback');
+      // },
+      checkCallback: (checking: boolean) => {
+        let leaf = this.app.workspace.activeLeaf;
+        if (leaf) {
+          if (!checking) {
+            new SampleModal(this.app).open();
+          }
+          return true;
+        }
+        return false;
+      },
+    });
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
+    this.registerCodeMirror((cm: CodeMirror.Editor) => {
+      console.log("codemirror", cm);
+    });
 
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+      console.log("click", evt);
+    });
 
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    this.registerInterval(
+      window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+    );
+  }
 
-	onunload() {
-		console.log('unloading plugin');
-	}
+  files: TFile[];
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  init() {
+    let nameContentArr: nameContent[];
+    let files: TFile[] = this.app.vault.getMarkdownFiles();
+    files.forEach(async (file) => {
+      const eachContent = await this.app.vault.cachedRead(file);
+      nameContentArr.push({ fileName: file.basename, content: eachContent });
+    });
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    // Regex to match the `parent` metadata field
+    const parentField = "yz-parent";
+    const yamlOrInlineParent = new RegExp(`${parentField}::? (.+)`, "i");
+  }
+  // Grab parent fields from note content
+  // Currently, this doesn't wait until the cachedRead is complete for all files
+
+  test(nameContentArr: nameContent[], yamlOrInlineParent: RegExp) {
+    let childParentArr = nameContentArr.map((arr: string[]) => {
+      const matches = arr[1].match(yamlOrInlineParent);
+      if (matches) {
+        const dropBrackets = matches[1].replace("[[", "").replace("]]", "");
+        return [arr[0], dropBrackets];
+      } else {
+        return [arr[0], ""];
+      }
+    });
+  }
+  // Graph stuff...
+
+  initialiseGraph(data: string[][]) {
+    let g = new Graph();
+    g.setNode("Index", "Index");
+
+    data.forEach((edge) => g.setNode(edge[0], edge[0]));
+
+    data.forEach((edge) => {
+      if (edge[1] !== "") {
+        g.setEdge(...edge, "child");
+      }
+    });
+  }
+
+  getBreadcrumbs(g: Graph, from: string, to: string = "Index") {
+    const paths = graphlib.alg.dijkstra(g, from);
+    let step = to;
+    const breadcrumbs: string[] = [];
+
+    while (paths[step].predecessor !== from) {
+      breadcrumbs.push(step);
+      step = paths[step].predecessor;
+    }
+
+    breadcrumbs.push(from);
+    return breadcrumbs;
+  }
+
+  onunload() {
+    console.log("unloading plugin");
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+  constructor(app: App) {
+    super(app);
+  }
 
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  onOpen() {
+    let { contentEl } = this;
+    contentEl.setText("Woah!");
+  }
 
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
+  onClose() {
+    let { contentEl } = this;
+    contentEl.empty();
+  }
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+  plugin: BreadcrumbsPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: BreadcrumbsPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		let {containerEl} = this;
+  display(): void {
+    let { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+    containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName("Setting #1")
+      .setDesc("It's a secret")
+      .addText((text) =>
+        text
+          .setPlaceholder("Enter your secret")
+          .setValue("")
+          .onChange(async (value) => {
+            console.log("Secret: " + value);
+            this.plugin.settings.mySetting = value;
+            await this.plugin.saveSettings();
+          })
+      );
+  }
 }
