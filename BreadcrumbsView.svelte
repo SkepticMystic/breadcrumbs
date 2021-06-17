c6ec2f2a860f6fe357eb2c4c865aedd25c5d24c9
Ross Keenan <rossk29@gmail.com>
2021-06-17 16:41:19 +0200

    Svelte attempt

---
+++ BreadcrumbsView.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let levels: string[] = ['1', '2'];
+</script>
+
+<div class="breadcrumb-trail">
+  {#each levels as crumb}
+    <span class="crumb">{crumb}</span>
+  {/each}
+</div>
--- main.ts
+++ main.ts
@@ -1,13 +1,23 @@
-import { App, Modal, Notice, Plugin, TFile } from "obsidian";
+import {
+  App,
+  EventRef,
+  ItemView,
+  Modal,
+  Notice,
+  Plugin,
+  TFile,
+  WorkspaceLeaf,
+} from "obsidian";
 import { Graph } from "graphlib";
 import * as graphlib from "graphlib";
 import { SampleSettingTab } from "./SampleSettingTab";
+import BreadcrumbsComponent from "BreadcrumbsView.svelte";
 
-interface MyPluginSettings {
+interface BreadcrumbsPluginSettings {
   mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: BreadcrumbsPluginSettings = {
   mySetting: "default",
 };
 
@@ -21,8 +31,43 @@ interface childParent {
   parent: string;
 }
 
+const VIEW_TYPE_BREADCRUMBS = "breadcrumbs";
+class BreadcrumbsView extends ItemView {
+  plugin: BreadcrumbsPlugin;
+  changeRef: EventRef = null;
+  private view: BreadcrumbsComponent;
+
+  constructor(leaf: WorkspaceLeaf, plugin: BreadcrumbsPlugin) {
+    super(leaf);
+    this.plugin = plugin;
+  }
+
+  onload() {
+    super.onload();
+  }
+
+  getViewType(): string {
+    return VIEW_TYPE_BREADCRUMBS;
+  }
+
+  getDisplayText(): string {
+    return "Breadcrumbs";
+  }
+
+  async onOpen(): Promise<void> {
+    this.view = new BreadcrumbsComponent({
+      target: this.contentEl,
+      props: {
+        levels: ["a", "b", "c"],
+      },
+    });
+  }
+}
+
 export default class BreadcrumbsPlugin extends Plugin {
-  settings: MyPluginSettings;
+  settings: BreadcrumbsPluginSettings;
+  private view: BreadcrumbsView;
+  plugin: BreadcrumbsPlugin;
 
   async onload() {
     console.log("loading plugin");
@@ -41,13 +86,19 @@ export default class BreadcrumbsPlugin extends Plugin {
 
     this.addSettingTab(new SampleSettingTab(this.app, this));
 
+    this.registerView(
+      "breadcrumbs",
+      (leaf: WorkspaceLeaf) =>
+        (this.view = new BreadcrumbsView(leaf, this.plugin))
+    );
+
     // this.registerDomEvent(document, "click", (evt: MouseEvent) => {
     //   console.log("click", evt);
     // });
 
-    this.registerInterval(
-      window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-    );
+    // this.registerInterval(
+    //   window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+    // );
   }
 
   async getNameContentArr(): Promise<nameContent[]> {
