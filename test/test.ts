1be789ea281ad84e20922a5a817dc42f71f24315
Ross Keenan <rossk29@gmail.com>
2021-11-24 16:26:34 +0200

    test: :test_tube: getReflexiveClosure runs and fails!

--- test/test.ts
+++ test/test.ts
@@ -3,35 +3,33 @@ import { MultiGraph } from "graphology";
 import { myFunc } from "../src/utils";
 // import { DIRECTIONS } from "../src/constants";
 
-import { getOppDir } from "../src/graphUtils";
+import { getOppDir, getReflexiveClosure } from "../src/graphUtils";
 
 require("approvals").mocha();
 
 describe("When running some tests", function () {
   it("should be able to use Approvals", function () {
-    const g = new MultiGraph();
-    console.log(getOppDir("up"));
     // const data = myFunc("Hello World!" + DIRECTIONS[0]);
     const data = myFunc("Hello World!");
-    // const userHiers = [
-    //   {
-    //     down: ["down"],
-    //     next: ["next"],
-    //     prev: ["prev"],
-    //     same: ["same"],
-    //     up: ["up"],
-    //   },
-    // ];
+    const userHiers = [
+      {
+        down: ["down"],
+        next: ["next"],
+        prev: ["prev"],
+        same: ["same"],
+        up: ["up"],
+      },
+    ];
 
-    // const g = new MultiGraph();
-    // g.addNode("A");
-    // g.addNode("B");
-    // g.addNode("C");
+    const g = new MultiGraph();
+    g.addNode("A");
+    g.addNode("B");
+    g.addNode("C");
 
-    // g.addEdge("A", "B", { dir: "up", field: "parent" });
-    // g.addEdge("C", "A", { dir: "prev", field: "previous" });
+    g.addEdge("A", "B", { dir: "up", field: "parent" });
+    g.addEdge("C", "A", { dir: "prev", field: "previous" });
 
-    // getReflexiveClosure(g, userHiers);
+    getReflexiveClosure(g, userHiers);
 
     this.verify(data); // or this.verifyAsJSON(data)
   });
