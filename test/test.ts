e7da7f1c4eeb561d9cfd1b8846a75b1544eaa24d
Ross Keenan <rossk29@gmail.com>
2021-11-25 13:51:42 +0200

    test: Remove default testing + fix line endings

--- test/test.ts
+++ test/test.ts
@@ -1,16 +1,10 @@
 import { MultiGraph } from "graphology";
-// import { getReflexiveClosure } from "src/sharedFunctions";
-import { myFunc } from "../src/utils";
-// import { DIRECTIONS } from "../src/constants";
-
-import { getOppDir, getReflexiveClosure } from "../src/graphUtils";
+import { getReflexiveClosure } from "../src/graphUtils";
 
 require("approvals").mocha();
 
 describe("When running some tests", function () {
   it("should be able to use Approvals", function () {
-    // const data = myFunc("Hello World!" + DIRECTIONS[0]);
-    const data = myFunc("Hello World!");
     const userHiers = [
       {
         down: ["down"],
@@ -24,14 +18,20 @@ describe("When running some tests", function () {
     const g = new MultiGraph();
     g.addNode("A");
     g.addNode("B");
-    g.addNode("C");
+    // g.addNode("C");
 
-    g.addEdge("A", "B", { dir: "up", field: "parent" });
-    g.addEdge("C", "A", { dir: "prev", field: "prev" });
+    g.addEdge("A", "B", { dir: "up", field: "up" });
+    // g.addEdge("C", "A", { dir: "prev", field: "prev" });
 
-    getReflexiveClosure(g, userHiers);
+    const closed = getReflexiveClosure(g, userHiers);
 
-    this.verify(data); // or this.verifyAsJSON(data)
+    const gStr = closed.inspect();
+    this.verifyAsJSON(gStr, {
+      reporters: ["tortoisemerge"],
+      appendEOL: true,
+      // Doesn't add \n
+      normalizeLineEndingsTo: "\n",
+    });
   });
 });
 
