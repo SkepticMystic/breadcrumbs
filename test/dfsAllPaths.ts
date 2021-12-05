c876954873d1cbd384f3aac5e64a9ae8a68f70d1
Ross Keenan <rossk29@gmail.com>
2021-12-05 11:58:16 +0200

    test: :white_check_mark: Passing :)

---
+++ test/dfsAllPaths.ts
@@ -0,0 +1,41 @@
+import { MultiGraph } from "graphology";
+import { dfsAllPaths } from "../src/graphUtils";
+import { testGraph } from "./testUtils";
+
+require("approvals").mocha();
+
+function verify(sol: any) {
+  this.verifyAsJSON(sol, {
+    reporters: ["tortoisemerge"],
+    appendEOL: true,
+    normalizeLineEndingsTo: "\r\n",
+  });
+}
+
+describe("dfsAllPaths", function () {
+  it("1", function () {
+    const g = new MultiGraph();
+    g.addNode("A");
+    g.addNode("B");
+
+    g.addEdge("A", "B", { dir: "up", field: "up" });
+
+    const paths = dfsAllPaths(g, "A");
+
+    verify.call(this, paths);
+    // this.verifyAsJSON(paths, {
+    //   reporters: ["tortoisemerge"],
+    //   appendEOL: true,
+    //   normalizeLineEndingsTo: "\r\n",
+    // });
+  });
+
+  it("2", function () {
+    const g = testGraph();
+    const paths = dfsAllPaths(g, "a");
+
+    verify.call(this, paths);
+  });
+});
+
+export {};
---
+++ test/getReflexiveClosure.ts
@@ -0,0 +1,25 @@
+import { MultiGraph } from "graphology";
+import { getReflexiveClosure } from "../src/graphUtils";
+import { testHiers, verify } from "./testUtils";
+
+require("approvals").mocha();
+
+describe("getReflexiveClosure", function () {
+  it("1", async function () {
+    const userHiers = testHiers();
+
+    const g = new MultiGraph();
+    g.addNode("A");
+    g.addNode("B");
+
+    g.addEdge("A", "B", { dir: "up", field: "up" });
+
+    const closed = getReflexiveClosure(g, userHiers);
+
+    const gStr = closed.inspect();
+
+    verify.call(this, gStr);
+  });
+});
+
+export {};
