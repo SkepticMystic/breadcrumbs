a73354bd9cbd995ad5e797220cc2365083c0e814
Ross Keenan <rossk29@gmail.com>
2022-02-12 11:36:48 +0200

    chore: :memo: Update docs

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,9 @@ export {
 export { DIRECTIONS, ARROW_DIRECTIONS } from "./constants";
 
 export { BCAPI } from "./API";
+export type {
+  BCAPII,
+  RealNImplied,
+  Directions,
+  SquareItem,
+} from "./interfaces";
