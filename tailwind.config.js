d9414a7576609fd2bcad6d0e939cde0f09898d31
SkepticMystic <rossk29@gmail.com>
2024-01-13 19:53:54 +0100

    TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	// NOTE: I removed html and js, since I don't think I'll need that here
+	content: ["./src/**/*.{svelte,ts}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
