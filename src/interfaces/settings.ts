25f8d1550252c8870798f2c90183440d00eba140
SkepticMystic <rossk29@gmail.com>
2024-01-24 14:36:39 +0100

    Add more to migrate_old_settings

--- src/interfaces/settings.ts
+++ src/interfaces/settings.ts
@@ -1,3 +1,4 @@
+import type { Direction } from "src/const/hierarchies";
 import type { Hierarchy } from "./hierarchies";
 
 export type ShowNodeOptions = {
@@ -7,6 +8,10 @@ export type ShowNodeOptions = {
 };
 
 export interface BreadcrumbsSettings {
+	// Once I've reach settings parity with old BC, I can add this flag to skip over all the checks in migrate_old_settings
+	// But for now, as I add new (old) settings, I don't want to skip migrating them
+	// V4_MIGRATED: boolean;
+
 	hierarchies: Hierarchy[];
 
 	explicit_edge_sources: {
@@ -48,3 +53,106 @@ export interface BreadcrumbsSettings {
 		};
 	};
 }
+
+export type OLD_BREADCRUMBS_SETTINGS = Partial<{
+	addDendronNotes: boolean; // MIGRATED
+	addDateNotes: boolean;
+	aliasesInIndex: boolean;
+	alphaSortAsc: boolean;
+	altLinkFields: string[];
+	CSVPaths: string;
+	createIndexIndent: string;
+	dvWaitTime: number;
+	dataviewNoteField: string;
+	debugMode: 0 | 1 | 2;
+	dendronNoteDelimiter: string; // MIGRATED
+	dendronNoteField: string; // MIGRATED
+	dateFormat: string;
+	dateNoteFormat: string;
+	dateNoteField: string;
+	dateNoteAddMonth: string;
+	dateNoteAddYear: string;
+	enableAlphaSort: boolean;
+	enableRelationSuggestor: boolean;
+	fieldSuggestor: boolean;
+	filterImpliedSiblingsOfDifferentTypes: boolean;
+	gridDefaultDepth: number;
+	hierarchyNotes: string[];
+	hierarchyNoteIsParent: boolean;
+	HNUpField: string;
+	/** WARNING: The defaults for this feature are all `false`! */
+	impliedRelations: {
+		/** Has it's own toggle already */
+		siblingIdentity: boolean;
+		/** TypeII implied (currently) */
+		sameParentIsSibling: boolean;
+		/** Traverse siblings horizontally to make all siblings siblings of each other */
+		siblingsSiblingIsSibling: boolean;
+		/** Your siblings' parents are your parents */
+		siblingsParentIsParent: boolean;
+		/** Aunt and Uncle */
+		parentsSiblingsIsParents: boolean;
+		/** Grandparents */
+		parentsParentsIsParent: boolean;
+		/** If two separate parents are siblings, their children are cousins */
+		cousinsIsSibling: boolean;
+	}; // MIGRATED
+	indexNotes: string[];
+	// Default layout to use for Juggl view
+	// jugglLayout: JugglLayouts;
+	/** An array of fields going _up_ which **will** be shown in the trail view */
+	limitTrailCheckboxes: string[];
+	/** An array of fields in all directions which **will** get written when running `Write implied BCs to file` */
+	limitWriteBCCheckboxes: string[];
+	limitJumpToFirstFields: string[];
+	CHECKBOX_STATES_OVERWRITTEN: boolean;
+	namingSystemField: string;
+	namingSystemRegex: string;
+	namingSystemSplit: string;
+	namingSystemEndsWithDelimiter: boolean;
+	noPathMessage: string;
+	openMatrixOnLoad: boolean;
+	// openStatsOnLoad: boolean;
+	openDuckOnLoad: boolean;
+	openDownOnLoad: boolean;
+	parseJugglLinksWithoutJuggl: boolean;
+	refreshOnNoteChange: boolean;
+	refreshOnNoteSave: boolean;
+	respectReadableLineLength: boolean; // MIGRATED
+	showAllPathsIfNoneToIndexNote: boolean;
+	showAllAliases: boolean;
+	showNameOrType: boolean;
+	showRelationType: boolean;
+	showWriteAllBCsCmd: boolean;
+	sortByNameShowAlias: boolean;
+	regexNoteField: string;
+	relSuggestorTrigger: string;
+	rlLeaf: boolean;
+	showBCs: boolean;
+	showBCsInEditLPMode: boolean;
+	showAll: string;
+	showGrid: boolean;
+	showImpliedRelations: boolean;
+	showUpInJuggl: boolean;
+	showJuggl: boolean;
+	showPrevNext: boolean; // MIGRATED
+	showRefreshNotice: boolean;
+	showTrail: boolean;
+	squareDirectionsOrder: (0 | 1 | 2 | 3 | 4)[];
+	tagNoteField: string;
+	threadIntoNewPane: boolean;
+	threadingTemplate: string;
+	threadingDirTemplates: { [dir in Direction]: string };
+	threadUnderCursor: boolean;
+	trailSeperator: string;
+	treatCurrNodeAsImpliedSibling: boolean;
+	trimDendronNotes: boolean;
+	useAllMetadata: boolean;
+	userHiers: Hierarchy["dirs"][];
+	// visGraph: VisType;
+	// visRelation: Relations;
+	// visClosed: string;
+	// visAll: string;
+	writeBCsInline: boolean;
+	wikilinkIndex: boolean;
+}>;
--- src/settings/migration.ts
+++ src/settings/migration.ts
@@ -1,42 +1,86 @@
 import type { Hierarchy } from "src/interfaces/hierarchies";
+import type {
+	BreadcrumbsSettings,
+	OLD_BREADCRUMBS_SETTINGS,
+} from "src/interfaces/settings";
 import type BreadcrumbsPlugin from "src/main";
 import { blank_hierarchy } from "src/utils/hierarchies";
 
 export const migrate_old_settings = async (plugin: BreadcrumbsPlugin) => {
-	const { settings } = plugin;
+	const settings = plugin.settings as BreadcrumbsSettings &
+		OLD_BREADCRUMBS_SETTINGS;
 
-	// Hierarchies used to just be the Record<Direction, string[]>, but now that's wrapped in an object
-	if (
-		settings.hierarchies.at(0) &&
-		settings.hierarchies.at(0)!.dirs === undefined
-	) {
-		// We can also handle the move of implied_relationships here
-		const old_settings = //@ts-ignore
-			settings.impliedRelations as {
-				siblingIdentity: boolean;
-				sameParentIsSibling: boolean;
-				siblingsSiblingIsSibling: boolean;
-				siblingsParentIsParent: boolean;
-				parentsSiblingsIsParents: boolean;
-				cousinsIsSibling: boolean;
-			};
-
+	// Hierarchies
+	/// Hierarchies used to just be the Record<Direction, string[]>, but it's now wrapped in an object
+	/// We can also handle the move of implied_relationships here
+	if (settings.userHiers && settings.impliedRelations) {
 		const implied_relationships: Hierarchy["implied_relationships"] = {
 			...blank_hierarchy().implied_relationships,
 
-			self_is_sibling: old_settings.siblingIdentity,
-			parents_sibling_is_parent: old_settings.parentsSiblingsIsParents,
-			cousing_is_sibling: old_settings.cousinsIsSibling,
-			same_parent_is_sibling: old_settings.sameParentIsSibling,
-			same_sibling_is_sibling: old_settings.siblingsSiblingIsSibling,
-			siblings_parent_is_parent: old_settings.siblingsParentIsParent,
+			self_is_sibling: settings.impliedRelations.siblingIdentity,
+			cousing_is_sibling: settings.impliedRelations.cousinsIsSibling,
+			same_parent_is_sibling:
+				settings.impliedRelations.sameParentIsSibling,
+			same_sibling_is_sibling:
+				settings.impliedRelations.siblingsSiblingIsSibling,
+			siblings_parent_is_parent:
+				settings.impliedRelations.siblingsParentIsParent,
+			parents_sibling_is_parent:
+				settings.impliedRelations.parentsSiblingsIsParents,
 		};
 
-		plugin.settings.hierarchies = settings.hierarchies.map((hierarchy) => ({
+		plugin.settings.hierarchies = settings.userHiers.map((hierarchy) => ({
+			dirs: hierarchy,
 			implied_relationships,
-			dirs: hierarchy as unknown as Hierarchy["dirs"],
 		}));
 
-		await plugin.saveSettings();
+		delete settings.userHiers;
+		delete settings.impliedRelations;
 	}
+
+	// Explicit edge sources
+	/// Dendron
+	if (
+		settings.addDendronNotes !== undefined &&
+		settings.dendronNoteField !== undefined &&
+		settings.trimDendronNotes !== undefined &&
+		settings.dendronNoteDelimiter !== undefined
+	) {
+		plugin.settings.explicit_edge_sources.dendron_note = {
+			enabled: settings.addDendronNotes,
+			default_field: settings.dendronNoteField,
+			delimiter: settings.dendronNoteDelimiter,
+			display_trimmed: settings.trimDendronNotes,
+		};
+
+		delete settings.addDendronNotes;
+		delete settings.dendronNoteField;
+		delete settings.trimDendronNotes;
+		delete settings.dendronNoteDelimiter;
+	}
+
+	// Views
+	/// Page
+	if (settings.respectReadableLineLength !== undefined) {
+		plugin.settings.views.page.all.readable_line_width =
+			settings.respectReadableLineLength;
+
+		delete settings.respectReadableLineLength;
+	}
+
+	//// Grid
+	if (settings.showGrid !== undefined) {
+		plugin.settings.views.page.grid.enabled = settings.showGrid;
+
+		delete settings.showGrid;
+	}
+
+	//// Prev/Next
+	if (settings.showPrevNext !== undefined) {
+		plugin.settings.views.page.prev_next.enabled = settings.showPrevNext;
+
+		delete settings.showPrevNext;
+	}
+
+	await plugin.saveSettings();
 };
