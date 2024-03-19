1fd728186e6d4d0e7ced374995b8c44c39e243aa
SkepticMystic <rossk29@gmail.com>
2024-03-19 08:50:53 +0200

    Touchup Matrix styling

--- src/components/TreeView.svelte
+++ src/components/TreeView.svelte
@@ -34,7 +34,7 @@
 
 <div class="markdown-rendered BC-tree-view">
 	<div class="BC-tree-view-controls flex justify-between">
-		<!-- TODO(#407): Add EdgeSortIdSelector to change sort order on the fly -->
+		<!-- TODO(#407, #261): Add EdgeSortIdSelector to change sort order on the fly -->
 		<span></span>
 
 		<label>
