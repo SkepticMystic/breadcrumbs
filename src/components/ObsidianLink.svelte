59f45f17124bbe44dd8a67760dd47e6cfb459821
SkepticMystic <rossk29@gmail.com>
2024-03-23 12:16:08 +0200

    Progress on #511

--- src/components/ObsidianLink.svelte
+++ src/components/ObsidianLink.svelte
@@ -19,15 +19,22 @@
 	class:BC-active-note={$active_file_store?.path === path}
 	href={no_ext}
 	data-href={no_ext}
-	aria-label={path === display ? "" : path}
+	aria-label={// Previously checked if path === display
+	// But I think we should only show the label if the folder/basename is different,
+	// not just the extension
+	no_ext === display ? "" : path}
 	on:click={(e) => {
 		// TODO: There seems to be an Obsidian issue here...
 		// I try to open /b from /a, but it opens /folder/b
-		// TODO: I think #511 comes from here. I think the third of fourth agurment should change if a second window is open
+		// TODO: I think #511 comes from here. Test if this does what is expected:
+		//  - If only one leaf is open, open the link in that leaf
+		//  - If multiple leaves are open, open in the active leaf
+		//  - If another window is open, open in the active window
 		plugin.app.workspace.openLinkText(
 			path,
 			$active_file_store?.path ?? "",
 			Keymap.isModEvent(e),
+			{ group: plugin.app.workspace.getLeaf(false) },
 		);
 	}}
 >
