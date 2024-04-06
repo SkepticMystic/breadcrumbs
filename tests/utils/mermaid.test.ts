51ebd23253d883de072609a47512d74b2745e9f9
SkepticMystic <rossk29@gmail.com>
2024-04-06 22:14:19 +0200

    test: Update mermaid tests based on renderer changes

--- tests/utils/mermaid.test.ts
+++ tests/utils/mermaid.test.ts
@@ -6,14 +6,15 @@ describe("from_edges", () => {
 	test("should create a mermaid flowchart from edges", (t) => {
 		t.expect(
 			Mermaid.from_edges([
-				_mock_edge("a", "b", { explicit: true, field: "up" }),
-				_mock_edge("b", "c", { explicit: false, field: "down" }),
+				_mock_edge("a.md", "b.md", { explicit: true, field: "up" }),
+				_mock_edge("b.md", "c.md", { explicit: false, field: "down" }),
 			]).trim(),
 		).toBe(
 			`
+%%{init: {"flowchart": {"defaultRenderer": "dagre"}} }%%
 flowchart LR
-\ta["a"] -->|up| b["b"]
-\tb["b"] -.->|down| c["c"]`.trimStart(),
+\ta.md("a") -->|up| b.md("b")
+\tb.md("b") -.->|down| c.md("c")`.trimStart(),
 		);
 	});
 });
