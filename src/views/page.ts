df0f51ac295833b2c4e8f4e4f0020d9945758a56
SkepticMystic <rossk29@gmail.com>
2024-04-12 16:00:44 +0200

    fix(view:page): Redraw the Page Views on all markdown views, not just the active one

--- src/views/page.ts
+++ src/views/page.ts
@@ -1,68 +1,81 @@
 import { MarkdownView } from "obsidian";
-import PageViews from "src/components/page_views/index.svelte";
+import PageViewsComponent from "src/components/page_views/index.svelte";
 import { log } from "src/logger";
 import type BreadcrumbsPlugin from "src/main";
 
 export const redraw_page_views = (plugin: BreadcrumbsPlugin) => {
-	const markdown_view =
-		plugin.app.workspace.getActiveViewOfType(MarkdownView);
-	if (!markdown_view) {
-		return log.info("redraw_page_views > No active markdown view");
+	const markdown_views = plugin.app.workspace.getLeavesOfType("markdown");
+	if (!markdown_views.length) {
+		log.info("redraw_page_views > No markdown views found");
+		return;
 	}
 
-	const markdown_view_mode = markdown_view.getMode();
+	markdown_views.forEach((leaf) => {
+		if (!(leaf.view instanceof MarkdownView)) return;
 
-	// Ensure the container exists _on the current page_, leaving other pages' containers alone
-	const page_views_el =
-		markdown_view.containerEl.querySelector(".BC-page-views") ??
-		markdown_view.containerEl.createDiv({
-			cls: "BC-page-views w-full mx-auto",
-		});
+		const markdown_view = leaf.view;
+		const mode = markdown_view.getMode();
 
-	// Set the max width
-	// NOTE: Do this _after_ getting the element
-	//   So that if it existed already, it gets updated
-	const max_width = plugin.settings.views.page.all.readable_line_width
-		? "var(--file-line-width)"
-		: "none";
-	page_views_el.setAttribute("style", `max-width: ${max_width};`);
+		// Ensure the container exists _on the current page_, leaving other pages' containers alone
+		const page_views_el =
+			markdown_view.containerEl.querySelector(".BC-page-views") ??
+			markdown_view.containerEl.createDiv({
+				cls: "BC-page-views w-full mx-auto",
+			});
 
-	// Stickyness
-	page_views_el.classList.toggle(
-		"BC-page-views-sticky",
-		plugin.settings.views.page.all.sticky,
-	);
+		// Set the max width
+		// NOTE: Do this _after_ getting the element
+		//   So that if it existed already, it gets updated
+		const max_width = plugin.settings.views.page.all.readable_line_width
+			? "var(--file-line-width)"
+			: "none";
+		page_views_el.setAttribute("style", `max-width: ${max_width};`);
 
-	// Clear out any old content
-	page_views_el.empty();
-
-	// Move it to the right place
-	if (markdown_view_mode === "preview") {
-		// NOTE: Embedded notes also match ".markdown-preview-view", so instead
-		//   we ensure the immediate parent is ".markdown-reading-view", which doesn't
-		//   exist on embedded notes
-		const view_parent = markdown_view.containerEl.querySelector(
-			".markdown-reading-view > .markdown-preview-view",
+		// Stickyness
+		page_views_el.classList.toggle(
+			"BC-page-views-sticky",
+			plugin.settings.views.page.all.sticky,
 		);
-		if (!view_parent) return log.info("redraw_page_views > No view_parent");
 
-		view_parent.insertBefore(page_views_el, view_parent.firstChild);
-	} else {
-		const view_parent =
-			markdown_view.containerEl.querySelector(".cm-scroller");
-		if (!view_parent) return log.info("redraw_page_views > No view_parent");
+		// Clear out any old content
+		page_views_el.empty();
 
-		// See here for an in-depth discussion on why it's done this way:
-		// https://discord.com/channels/686053708261228577/931552763467411487/1198377191994564621
-		// But basically, this shouldn't affect anything, and it's by far the easiest way to do it
-		view_parent.addClass("flex-col");
+		// Move it to the right place
+		if (mode === "preview") {
+			// NOTE: Embedded notes also match ".markdown-preview-view", so instead
+			//   we ensure the immediate parent is ".markdown-reading-view", which doesn't
+			//   exist on embedded notes
+			const view_parent = markdown_view.containerEl.querySelector(
+				".markdown-reading-view > .markdown-preview-view",
+			);
+			if (!view_parent) {
+				return log.info(
+					"redraw_page_views > No view_parent (mode=preview)",
+				);
+			}
 
-		view_parent.insertBefore(page_views_el, view_parent.firstChild);
-	}
+			view_parent.insertBefore(page_views_el, view_parent.firstChild);
+		} else {
+			const view_parent =
+				markdown_view.containerEl.querySelector(".cm-scroller");
+			if (!view_parent) {
+				return log.info(
+					"redraw_page_views > No view_parent (mode=source)",
+				);
+			}
 
-	// Render the component into the container
-	new PageViews({
-		target: page_views_el,
-		props: { plugin },
+			// See here for an in-depth discussion on why it's done this way:
+			// https://discord.com/channels/686053708261228577/931552763467411487/1198377191994564621
+			// But basically, this shouldn't affect anything, and it's by far the easiest way to do it
+			view_parent.addClass("flex-col");
+
+			view_parent.insertBefore(page_views_el, view_parent.firstChild);
+		}
+
+		// Render the component into the container
+		new PageViewsComponent({
+			target: page_views_el,
+			props: { plugin, file_path: markdown_view.file?.path ?? "" },
+		});
 	});
 };
