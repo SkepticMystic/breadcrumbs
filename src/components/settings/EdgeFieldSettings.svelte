e1665624e1d9dcb0cec5536ea8b27845ca0dedc0
SkepticMystic <rossk29@gmail.com>
2024-04-14 15:38:10 +0200

    EdgeFieldSettings: dirty flag + rename_field

--- src/components/settings/EdgeFieldSettings.svelte
+++ src/components/settings/EdgeFieldSettings.svelte
@@ -4,9 +4,13 @@
 
 	export let plugin: BreadcrumbsPlugin;
 
+	let dirty = false;
+
 	const actions = {
 		save: async () => {
 			await Promise.all([plugin.saveSettings(), plugin.refresh()]);
+
+			dirty = false;
 		},
 
 		fields: {
@@ -14,6 +18,8 @@
 				plugin.settings.edge_fields.push({
 					label: "New Edge Field",
 				});
+
+				dirty = true;
 				plugin = plugin;
 			},
 			remove: (edge_field: EdgeField) => {
@@ -28,6 +34,7 @@
 					);
 				});
 
+				dirty = true;
 				plugin = plugin;
 			},
 
@@ -41,6 +48,7 @@
 					group.fields[index] = new_label;
 				});
 
+				dirty = true;
 				plugin = plugin;
 			},
 		},
@@ -51,6 +59,8 @@
 					label: "New Group",
 					fields: [],
 				});
+
+				dirty = true;
 				plugin = plugin;
 			},
 
@@ -60,16 +70,21 @@
 						(g) => g.label !== group.label,
 					);
 
+				dirty = true;
 				plugin = plugin;
 			},
 
 			add_field: (group: EdgeFieldGroup, field_label: string) => {
 				group.fields.push(field_label);
+
+				dirty = true;
 				plugin = plugin;
 			},
 
 			remove_field: (group: EdgeFieldGroup, field_label: string) => {
 				group.fields = group.fields.filter((f) => f !== field_label);
+
+				dirty = true;
 				plugin = plugin;
 			},
 		},
@@ -78,13 +93,18 @@
 
 <div class="flex flex-col gap-5">
 	<div class="flex flex-wrap gap-3">
-		<button on:click={() => actions.fields.add()}> New Edge Field </button>
+		<button on:click={actions.fields.add}> New Edge Field </button>
 
-		<button on:click={() => actions.groups.add()}> New Group </button>
+		<button on:click={actions.groups.add}> New Group </button>
 
 		<button on:click={actions.save}> Save </button>
+
+		{#if dirty}
+			<span class="text-warning"> Remember to save your changes! </span>
+		{/if}
 	</div>
 
+	<!-- Fields -->
 	<div class="flex flex-col gap-3">
 		{#each plugin.settings.edge_fields as edge_field, i}
 			{@const group_labels = plugin.settings.edge_field_groups
@@ -92,7 +112,15 @@
 				.map((g) => g.label)}
 
 			<div>
-				<input type="text" bind:value={edge_field.label} />
+				<input
+					type="text"
+					on:blur={(e) => {
+						actions.fields.rename(
+							edge_field,
+							e.currentTarget.value,
+						);
+					}}
+				/>
 
 				<button on:click={() => actions.fields.remove(edge_field)}>
 					Delete
@@ -109,23 +137,26 @@
 		{/each}
 	</div>
 
+	<!-- Groups -->
 	<div class="flex flex-col gap-3">
 		{#each plugin.settings.edge_field_groups as group}
 			<div>
 				<input type="text" bind:value={group.label} />
 
-				<div class="flex flex-wrap gap-2">
+				<div class="flex flex-wrap gap-3">
 					{#each group.fields as field}
-						<span class="tag">
-							{field}
-						</span>
+						<divf class="flex gap-1">
+							<span class="tag">
+								{field}
+							</span>
 
-						<button
-							on:click={() =>
-								actions.groups.remove_field(group, field)}
-						>
-							Delete
-						</button>
+							<button
+								on:click={() =>
+									actions.groups.remove_field(group, field)}
+							>
+								Delete
+							</button>
+						</divf>
 					{/each}
 
 					<select
