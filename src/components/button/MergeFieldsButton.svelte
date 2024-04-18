1cfc696dd3b605bd05e8af8aa45d45f7072ff488
SkepticMystic <rossk29@gmail.com>
2024-04-18 16:59:57 +0200

    fix: Button icons show their current state, not where they're going

--- src/components/button/MergeFieldsButton.svelte
+++ src/components/button/MergeFieldsButton.svelte
@@ -12,8 +12,8 @@
 	on:click={() => (merge_fields = !merge_fields)}
 >
 	{#if merge_fields}
-		<SplitIcon size={ICON_SIZE} />
-	{:else}
 		<MergeIcon size={ICON_SIZE} />
+	{:else}
+		<SplitIcon size={ICON_SIZE} />
 	{/if}
 </button>
